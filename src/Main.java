359e47fc477dff5bfb7e9dd4e79eb00d0e484dd9
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2026-02-05 13:29:54 +0000

    Initial Commit for game

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+        background(0);
+    }
+
+    public void draw() {
+        ellipse(mouseX, mouseY, 50, 50);
+    }
+}
